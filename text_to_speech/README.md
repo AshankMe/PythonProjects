0b06300fbf549e63b3663dc6332f443a0333e3c1
AshankMe <86132517+SlippiDev@users.noreply.github.com>
2022-02-27 19:55:01 -0800

    Added TextToSpeech to Repo

---
+++ text_to_speech/README.md
@@ -0,0 +1,7 @@
+# Text To Speech
+
+This file takes a typed input from the user and then says it out loud. 
+
+Completely made by me.
+
+You can change the accent in the 'lang' variable.
---
+++ text_to_speech/texttospeech.py
@@ -0,0 +1,11 @@
+from gtts import gTTS
+import os 
+import playsound as p
+speech = input('Enter the sentence you want to say.')
+if speech == 'How are you?':
+    text1 = 'I am fine, what about you?'
+text1 = speech
+language = 'en'
+speak = gTTS(text =  text1,lang = language, slow = False)
+speak.save('Welcome.mp3')
+p.playsound('Welcome.mp3')
