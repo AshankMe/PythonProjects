6713baf01113d28c117baf80a32f2412ad8579b6
AshankMe <86132517+SlippiDev@users.noreply.github.com>
2022-02-28 08:22:43 -0800

    calc in repo

---
+++ calculator/README.md
@@ -0,0 +1,5 @@
+# Calculator
+
+This calculator is not complete, therefore it will not run properly until completed. 
+
+This is made by me, and only me.
---
+++ calculator/calculator.py
@@ -0,0 +1,9 @@
+# Calculator Program    
+
+print("Welcome! This is the PCC, also known as the Python Coded Calculator. All you have to do, is enter in what you want to add, subtract, divide, or multiply, and this calculator will print the answer.")
+print("--------------------------------------------------------------------------------------------")
+
+def calculate():
+    type_of_calculation = str(input("Enter the Method of Calculation -->"))
+    if type_of_calculation.lower() == 'addition':
+        print("bye")
