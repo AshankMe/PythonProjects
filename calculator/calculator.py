76160b673035f729680a45cb8399db7cf50eb6ec
AshankMe <86132517+SlippiDev@users.noreply.github.com>
2022-04-02 11:35:11 -0700

    Update calculator.py

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -5,11 +5,12 @@ print("-------------------------------------------------------------------------
 
 
 def addition():
-    val1 = input("Enter the first value -->")
-    val2 = input("Enter the second value -->")
+    val1 = int(input("Enter the first value -->"))
+    val2 = int(input("Enter the second value -->"))
     sum = val1 + val2
     print("The total of the 2 values is {}!".format(sum))
-    
+def subtraction():
+    val1 = int(input())
 def calculate():
     type_of_calculation = str(input("Enter the Method of Calculation -->"))
     if type_of_calculation.lower() == 'addition':
