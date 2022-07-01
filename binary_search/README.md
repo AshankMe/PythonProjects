96b4eaa1b324b35edcf6386192a584a1487a19d9
SlippyDev <86132517+SlippiDev@users.noreply.github.com>
2022-07-01 09:27:45 -0700

    created binary search algorithm code

---
+++ binary_search/README.md
@@ -0,0 +1,4 @@
+# Binary Search
+
+This code is used to take a list and scan it for a target value.
+
---
+++ binary_search/binary_search.py
@@ -0,0 +1,21 @@
+from sys import exit
+total_count = int(input("Enter how many items your list will have --> "))
+array = []
+for i in range(total_count):
+    addition = input("Enter the value --> ")
+    array.append(addition)
+print(array)
+target = input("Enter the target item --> ")
+count = 1
+target_found_count = 0
+count_list = []
+for i in array:
+    if i == target:
+        target_found_count += 1
+        count_list.append(count)
+    count += 1
+    if count == total_count and target_found_count == 0:
+        print("The target was not found in the array.")
+        exit()
+print(f"The target was found {target_found_count} times.")
+print(f"The target was found at index: {count_list}")
