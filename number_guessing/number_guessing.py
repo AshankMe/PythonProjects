7ff6d6acb02705578edf69f37bce753b419ea297
iSlxp <86132517+iSlxp@users.noreply.github.com>
2023-04-17 22:26:36 -0700

    teaching

--- number_guessing/number_guessing.py
+++ number_guessing/number_guessing.py
@@ -1,15 +1,13 @@
-import random 
+import random
 
-def guess(x):
-    random_number = random.randint(1,x)
-    guess = 349349384039 
-    while guess != random_number:
-        guess = int(input(f'Guess a Number between 1 and {x}!'))
-        if guess < random_number:
-            print('Your Guess is a little low! Try guessing higher.')
-        elif guess > int(random_number):
-            print('Your guess is too high! Try guessing lower.')
-        
-    print('You Nailed It!')
-guess(5)
-    
+number = str(random.randint(1,9999))
+
+person_guess = input("Enter the number: ")
+
+list_number = list(number)
+
+person_list = list(person_guess)
+
+
+
+
