dbea441ebdd77a4e4535d06efd6acccfa955a9a3
iSlxp <ashank.mehrotra@hotmail.com>
2024-03-18 21:43:25 -0700

    teaching this

--- shoplist_program/shoplist_code.py
+++ shoplist_program/shoplist_code.py
@@ -1,25 +1,41 @@
-print(" Prepare list of 3 items to shop")
-shoplist =[] #creating empty list named shoplist
-item1 = input("Enter first item")
-shoplist.append(item1)
-print(shoplist)
-shoplist.append(item1)
-item2 =input("Enter second item")
-shoplist.append(item2)
-item3 = input("Enter third item")
-shoplist.append(item3)
-print("The list prepared is ",shoplist)
-print("Prepare list of items you shoped from above removing from shoplisft and adding to new list say basket")
-basket=[]
-getitem1 = input("Enter one name you got from shop list items:")
-shoplist.remove(getitem1)
-basket.append(getitem1)
-print("Shoplist",shoplist,len(shoplist),"items","\n basket",basket,len(basket),'items')
-getitem2 = input("Enter one name except previously selected from items:")
-shoplist.remove(getitem2)
-basket.append(getitem2)
-print("Shoplist",shoplist,len(shoplist),"items","\n basket",basket,len(basket),'items')
-getitem3 = input("Enter one name except previously selected from items:")                       
-shoplist.remove(getitem3)                                                         
-basket.append(getitem3)
-print("Shoplist",shoplist,len(shoplist),"items","\n basket",basket,len(basket),'items')
+# Create List
+shopping_list = []
+# Print
+print("Add stuff to your list")
+# Input
+item1 = input("First item --> ")
+item2 = input("Second item --> ")
+item3 = input("Third item --> ")
+
+shopping_list.append(item1)
+shopping_list.append(item2)
+shopping_list.append(item3)
+
+print(f"Your shopping list is {shopping_list}!")
+
+
+# Remove stuff from list
+
+r1 = input("What would you like to remove first? ")
+try:
+    shopping_list.remove(r1) # tries this
+except:
+    print("That is not in your shopping list!") # exception
+print(f"Your shopping list is {shopping_list}!")
+r2 = input("What would you like to remove second? ")
+try:
+    shopping_list.remove(r2)
+except:
+    print("That is not in your shopping list!")
+print(f"Your shopping list is {shopping_list}!")
+r3 = input("What would you like to remove third? ")
+try:
+    shopping_list.remove(r3)
+except: 
+    print("That is not in your shopping list!")
+print(f"Your shopping list is {shopping_list}!")
+
+
+
+
+
