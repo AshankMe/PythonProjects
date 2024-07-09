fe046a9692e3273282d36da4e666566685b660af
iSlxp <ashank.mehrotra@hotmail.com>
2024-07-08 23:04:22 -0700

    worked on shoplist

--- shoplist_program/shoplist_code.py
+++ shoplist_program/shoplist_code.py
@@ -1,39 +1,34 @@
 # Create List
 shopping_list = []
-# Print
-print("Add stuff to your list")
-# Input
-item1 = input("First item --> ")
-item2 = input("Second item --> ")
-item3 = input("Third item --> ")
 
-shopping_list.append(item1)
-shopping_list.append(item2)
-shopping_list.append(item3)
+print("Welcome to your shopping list.")
 
-print(f"Your shopping list is {shopping_list}!")
+def add():
+    item = input("What would you like to add?")
+    shopping_list.append(item)
+
+    print(f"Your shopping list has {shopping_list}!")
+
+def remove():
+    r1 = input("What would you like to remove first? ")
+    try:
+        shopping_list.remove(r1) # tries this
+    except:
+        print("That is not in your shopping list!") # exception
+    print(f"Your shopping list has {shopping_list}!")
 
 
-# Remove stuff from list
+exit = False
+while exit != True:
+    q = input("What would you like to do? (Add, Remove, Exit) ").lower()
 
-r1 = input("What would you like to remove first? ")
-try:
-    shopping_list.remove(r1) # tries this
-except:
-    print("That is not in your shopping list!") # exception
-print(f"Your shopping list is {shopping_list}!")
-r2 = input("What would you like to remove second? ")
-try:
-    shopping_list.remove(r2)
-except:
-    print("That is not in your shopping list!")
-print(f"Your shopping list is {shopping_list}!")
-r3 = input("What would you like to remove third? ")
-try:
-    shopping_list.remove(r3)
-except: 
-    print("That is not in your shopping list!")
-print(f"Your shopping list is {shopping_list}!")
+    if q == "add":
+        add()
+    elif q == "remove":
+        remove()
+    elif q == "exit":
+        print("Goodbye!")
+        exit = True
 
 
 
